33da02e8939dbe157328cec6e77997b08c77010b
Salvador Rodriguez <chava.crb@gmail.com>
2024-05-14 15:11:38 -0600

    complete wizard atm

--- main.c
+++ main.c
@@ -1,84 +1,44 @@
-#include "./includes/functions.h" // Includes the functions.h file
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include "includes/functions.h"
 
-/*
- * compile tests:
- * gcc atm.c ./functions/functions.c -o ejec.exe
- * once we have all functions defined:
- * create an object file and execute the command
+int main()
+{
+  ATM bank;
+  bank.num_users = 0;
+  bank.num_transactions = 0;
+  bank.activeUser = -1;
 
-TODO:
-  * Define necessary structures to handle the ATM
-      * User
-      * Account
-      * Transaction (Deposit / Transfer / Withdraw)
-      * Receipt
-          * Receipt function executes right after a transaction is done.
+  int option;
 
-  * Define the functions to handle the ATM
-      (use the functions.h file to define the functions)
-      * Login function.
-      * Register user function.
-      * Deposit function.
-      * Withdraw function.
-      * Transfer function.
-      * Check balance function.
-      * Convert currency function.
-      * Print receipt function.
-      * log out function.
-
-  * Once we login or create a user the UserScreen shows up
-      * In the user screen we can:
-          * Deposit.
-          * Withdraw.
-          * Transfer.
-          * Check balance.
-          * Get the receipt.
-          * Log out.
-*/
-
-
-
-int main() {
-  int option, ret; // option and return value
-  ATM atm;
-mainMenu: // selection main menu
-
-  clearScreen();
-  printf("\n====================WIZARD BANK====================\n");
-
-  printf("\n\n\t1.Log-In to a Existing Account.\t");
-  printf("\n\n\t2.Create a New Account.\t");
-  printf("\n\n\t3.Exit.\t\n");
-  printf("\n\n=================================================");
-
-  // Select an option
-  printf("\n\n\tEnter your choice: ");
-  ret = scanf("%d", &option);
-
-  // Switch statement for the main menu
-  switch (option) {
-  case 1:
-    // log in to an existing account
-   
-    break;
-  case 2:
-    // create a new account
-    
-    atm.num_users = 0;
-    registerUser(&atm);
-    getchar();
-    break;
-  case 3:
-    // exit
+  while (1)
+  {
     clearScreen();
-    exitProgram();
-    break;
-  default:
-    printf("\nInvalid Selection\n\n");
-    goto mainMenu;
+    printf("\n====================WIZARD BANK====================\n");
+    printf("\n\n\t1.Log-In to a Existing Account.\t");
+    printf("\n\n\t2.Create a New Account.\t");
+    printf("\n\n\t3.Exit.\t\n");
+    printf("\n\n=================================================");
+    printf("\n\n\tEnter your choice: ");
+    scanf("%d", &option);
+    clearInputBuffer(); // Limpiar el buffer de entrada después de leer la opción
+
+    switch (option)
+    {
+    case 1:
+      // Implementar función de inicio de sesión
+      login(&bank);
+      break;
+    case 2:
+      registerUser(&bank);
+      break;
+    case 3:
+      printf("Thank you for using Wizard Bank. Goodbye!\n");
+      return 1;
+    default:
+      printf("Invalid option\n");
+      break;
+    }
   }
 
   return 0;
